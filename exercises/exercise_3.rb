10f608c96efa2998a906156c46fc38724620c665
Sneha Mahajan <sneh.km@gmail.com>
2022-08-25 17:03:47 -0400

    Ex3

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,14 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+### Exercise 3: Delete the third store
+
+# 1. Load the third store (into `@store3`) as you did the other two before.
+# 2. Using Active Record's `destroy` method, delete the store from the database.
+# 3. Verify that the store has been deleted by again outputting (`puts`ing) the `count` (as you did in Exercise 1.)
+
+@store3 = Store.find_by(name: "Gastown");
+
+@store3.destroy
+
+puts "#{Store.count} stores exist."
