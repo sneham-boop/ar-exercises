0d8cddbd346a1ab7041d0c805393a12bdd9617ae
Sneha Mahajan <sneh.km@gmail.com>
2022-08-25 17:43:53 -0400

    Ex5

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+# 1. Output the total revenue for the entire company (all stores), using Active Record's `.sum` calculation method.
+# 2. On the next line, also output the average annual revenue for all stores.
+# 3. Output the number of stores that are generating $1M or more in annual sales. **Hint:** Chain together `where` and `size` (or `count`) Active Record methods.
+
+puts "Total revenue is #{Store.sum('annual_revenue')}"
+puts "Average annual revenue is #{Store.sum('annual_revenue')/Store.count}"
+
+count_of_stores = Store.where('annual_revenue>1000000').count
+
+puts "#{count_of_stores} stores generate atleast 1000000 in revenue."
